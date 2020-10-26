15df1a4a2a1eccd5b21e3fba131a188dec30c965
Lamronba <72434638+Lamronba@users.noreply.github.com>
2020-10-26 20:17:30 +0800

    Activity 5

---
+++ Sotela/Acitivity 5/I-VI/iii/III.java
@@ -0,0 +1,24 @@
+
+package iii;
+
+public class III {
+
+    public static void main(String args[]) {
+       int n = 5;
+        for (int i=0;i<5;i++)
+        {
+          
+            for (int j=2*(n-i);j>=0;j--)
+            {
+               
+                System.out.print(" ");
+            }
+            for (int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
