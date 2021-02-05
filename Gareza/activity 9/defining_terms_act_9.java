559ec6c2d0d0133c5645c87c74c5af70631c929f
lukegareza <72480268+lukegareza@users.noreply.github.com>
2021-02-05 19:25:13 +0800

    activity 9 - Gareza

---
+++ Gareza/activity 9/defining_terms_act_9.java
@@ -0,0 +1,13 @@
+package lukegareza;
+
+public class defining_terms_act_9 {
+    public static void main(String[] args) {
+        System.out.println("1. class - for me class is somethat that differentiates variables from one another and class is an object constructor, or like an outline for creating objects.\n" +
+                "2. object - an object is an element or instance of a .\n" +
+                "3. instantiate - is when you want to call a certain class to run the code.\n" +
+                "4. instance variable -it is the declaration whether you want your variable to be public ,static or default.\n" +
+                "5. instance method - a non-static method which is declared inside a class.\n" +
+                "6. class variable or static member variable - an example of this is the company name of employees, college name of students, etc. The static variable gets memory only once in the class area at the time of class loading.\n" +
+                "7. constructor - is somthing to initialize the object of a class while the purpose of a method is to perform a task by executing java code. Constructors cannot be abstract, final, static and synchronised while methods can be.");
+    }
+}
