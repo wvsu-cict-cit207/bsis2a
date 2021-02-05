55d381b58d856ea8158c42981e5abfcbe8219ed2
lukegareza <72480268+lukegareza@users.noreply.github.com>
2021-02-05 19:29:33 +0800

    activity 11 - Gareza

---
+++ Gareza/activity 11/garezaModel.java
@@ -0,0 +1,19 @@
+package finals;
+
+public class garezaModel {
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
+    public static int min(int num1, int num2){
+        return num1 - num2;
+    }
+    public static int mul(int num1, int num2){
+        return num1 * num2;
+    }
+    public static double div(double num1, double num2){
+        return num1 / num2;
+    }
+    public static double quo(double num1,double num2){
+        return num1 % num2;
+    }
+}
